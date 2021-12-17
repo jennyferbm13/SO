0586f269e70245e16eb77680e4ee8eaec1d107cf
jennyferbm13 <51867672+jennyferbm13@users.noreply.github.com>
2021-12-16 22:20:20 -0500

    comentarios

--- leercadena_main.c
+++ leercadena_main.c
@@ -12,19 +12,25 @@
 #include "leercadena.h"
 #include <stdlib.h> 
 int main(int argc, char *argv[]) {
+//se declaran las variables iniciales
   char comando[BUFSIZ];
   char **vector;
   int i;
   char prueba[] = "quit";
   pid_t pid;
-
+//Se repite hasta que el proceso no pueda ser creado o se ingrese un comando 
+//prestablecido para detenerlo
   while(1) {
           printf("> ");
+	  //lee un comando del usuario y crea un proceso hijo para que lo ejecute
           leer_de_teclado(BUFSIZ,comando);
           pid = fork();
+	  //Si no se puede crear el hijo, imprime un mensaje y termina el programa
           if (pid < 0) {
                   printf("No pude crear un proceso\n");
                   return 2;
+	//El hijo intena ejecutar el comando ingreado por el usuario. Si el comando es el 
+        //prestablecido termina el proceso
           } else if (pid == 0) {
 		if((strcmp(comando,prueba))==0){
 			exit(0);}
@@ -34,7 +40,8 @@ int main(int argc, char *argv[]) {
 
              execvp(vector[0],vector);
 
-
+		//El padre espera a que la ejecucion del hijo termine. Si el comando es el
+                //prestablecido, termina el programa
           } else {
                   wait(NULL);
 	        if((strcmp(comando,prueba))==0){
